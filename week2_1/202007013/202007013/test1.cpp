8b7cfafb33a6ad1fd16250b962b101a81a24ac20
Dnut0121 <namgue0706@naver.com>
2023-09-12 11:37:15 +0900

    2주차

--- week2_1/202007013/202007013/test1.cpp
+++ week2_1/202007013/202007013/test1.cpp
@@ -7,25 +7,28 @@ int main()
 	char input;
 	char ch;
 
-	printf("202007013\n");
-	printf("이은석님의\n");
-	printf("Application\n");
+	printf("이은석의 행복한 VC++\n");
 
 
 	while (1) {
 		printf("command>");
-		input = _getch(); // 키 입력을 가져옴
 		scanf("\n%c", &input);
-		if (input == 'p') {
-			printf("나는 과제를 겁나 잘하는 학생이다.\n");
+		if (input == 'q') {
+			printf("qwer 화이팅!!\n");
 		}
-		else if (input == 'f') {
-			printf("나는 과제가 너무 즐겁다.\n");
+		else if (input == 'w') {
+			printf("과제 너무 좋다.\n");
 		}
-		else if (input == 'j') {
-			printf("교수님 과제 더 내주세요.\n");
+		else if (input == 'e') {
+			printf("담주부터 과제량 3배다.\n");
 		}
-		
+		else if (input == 'r') {
+			printf("행복합니다.\n");
+		}
+		else {
+			printf("잘못된 입력입니다.다시입력하세요.\n");
+		}
+		input = _getch(); // 키 입력을 가져옴
 			if (input == 27) { // Esc 키의 ASCII 코드는 27
 				printf("프로그램을 종료합니다.\n");
 				break; // 루프 종료
